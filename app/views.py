485b10c1609c104496b03b24aa6ab14d6b10ac75
j0hnsmith <info@whywouldwe.com>
2012-12-06 09:22:01 +0000

    use templates instead of direct html output

--- app/views.py
+++ app/views.py
@@ -1,13 +1,14 @@
+from flask import render_template
 from app import app, tasks
 
 @app.route('/')
 def home():
-    return 'Hello World!<br/><a href="/run-task-to-get-files">run task in background to get files</a>'
+    return render_template('base.html', h1='Hello World!', content='<a href="/run-task-to-get-files">run task in background to get files</a>')
 
 @app.route('/run-task-to-get-files')
 def run_task_to_get_files():
     tasks.get_local_files.apply_async()
-    return 'task added<br/><a href="/api/v1/files">view all files via api</a>'
+    return render_template('base.html', h1='Task Added', content='<a href="/api/v1/files">view all files via api</a>')
 
 
 
